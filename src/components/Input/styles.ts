c7a00326774bbccf7008bd14b43745ff8af3e869
Daniel Cunha <danielcc.jp@gmail.com>
2020-11-22 13:52:13 -0300

    + isolando componentes (button & input)

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: #232129;
+  border-radius: 10px;
+  border: 2px solid #232129;
+  padding: 16px;
+  width: 100%;
+  color: #666360;
+
+  display: flex;
+  align-items: center;
+
+  & + div {
+    margin-top: 8px;
+  }
+
+  input {
+    background: transparent;
+    flex: 1;
+    border: 0;
+    color: #f4ede8;
+
+    &::placeholder {
+      color: #666360;
+    }
+
+    &:-webkit-autofill,
+    &:-webkit-autofill:hover,
+    &:-webkit-autofill:focus,
+    &:-webkit-autofill:active {
+      box-shadow: inset 0 0 0px 9999px #232129;
+      -webkit-box-shadow: inset 0 0 0px 9999px #232129;
+      -webkit-text-fill-color: #f4ede8;
+    }
+  }
+
+  svg {
+    margin-right: 16px;
+  }
+`;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
@@ -14,10 +17,15 @@ const SignIn: React.FC = () => {
         <form autoComplete="off">
           <h1>Faça seu logon</h1>
 
-          <input autoComplete="off" type="text" placeholder="E-mail" />
-          <input autoComplete="off" type="password" placeholder="Senha" />
+          <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
 
-          <button type="submit">Entrar</button>
+          <Button type="submit">Entrar</Button>
 
           <a href="forgot">Esqueci minha senha</a>
         </form>
