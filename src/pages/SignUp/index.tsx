0e5478fb9d0cdf6011c90191661b059b3c696c58
Daniel Cunha <danielcc.jp@gmail.com>
2020-11-25 14:43:48 -0300

    + criando tooltip de erros

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -23,7 +23,7 @@ const SignUp: React.FC = () => {
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório'),
         email: Yup.string()
-          .required('E-mail orbigatório')
+          .required('E-mail obrigatório')
           .email('Digite um e-mail válido'),
         password: Yup.string().min(6, 'Mínimo de 6 dígitos'),
       });
